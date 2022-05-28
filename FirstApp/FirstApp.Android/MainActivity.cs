2fef8534218767f6ac71749c6240374757eb527d
Nurdaulet <monolithdestroyer@gmail.com>
2022-05-28 23:51:41 +0600

    changes

--- FirstApp/FirstApp.Android/MainActivity.cs
+++ FirstApp/FirstApp.Android/MainActivity.cs
@@ -23,6 +23,7 @@ namespace FirstApp.Droid
             Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
 
             base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+            //dasasddas
         }
     }
 }
